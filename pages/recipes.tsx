add1b10b6b09c365411f78823a29c1691cf9afb8
Irenee Ajeneza <irenee.ajeneza@primarybid.com>
2022-03-19 19:10:20 +0000

    Added AddRecipeForm component

--- pages/recipes.tsx
+++ pages/recipes.tsx
@@ -1,16 +1,22 @@
 import {NextPage, GetStaticProps} from "next";
 
-import PageLayout from "../components/PageLayout";
+import PageLayout from "../components/PageLayout/PageLayout";
 import {RecipeModel} from "../services/models/recipe.model";
 import {findAllRecipes} from "../services/store-functions/find-all-recipes";
+import {AddRecipeForm} from "../components/AddRecipeForm/AddRecipeForm";
 
 interface RecipesPros {
     recipes: RecipeModel[]
 }
 
 const Recipes: NextPage<RecipesPros> = ({recipes}) => {
+    const onAddRecipe = async (recipientName: string) => {
+        // TODO: call an api endpoint
+        return true;
+    }
     return (
         <PageLayout pageTitle="Recipes">
+            <AddRecipeForm onFormSubmit={onAddRecipe}/>
             <ul>
                 {recipes.map(recipe => (
                     <li key={recipe.id}>{recipe.name}</li>
