7aaf780ab9eadde188737cae6e389b56f7deca6c
Irenee Ajeneza <irenee.ajeneza@primarybid.com>
2022-04-09 12:37:43 +0100

    Delete recipe store function

--- services/recipe-api/delete-recipe.handler.ts
+++ services/recipe-api/delete-recipe.handler.ts
@@ -1,15 +1,20 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import {DeleteRecipeResponse} from "./delete-recipe.response";
 import {ApiError} from "../../libs/api-utils";
+import {deleteRecipe} from "../../store/store-functions";
 
 export const deleteRecipeHandler = async (req: NextApiRequest, res: NextApiResponse<DeleteRecipeResponse | ApiError>) => {
     try {
+        const recipeId = req.query.recipeId as string;
+        await deleteRecipe(recipeId );
+
         return res
-            .status(500)
-            .json({error: "Action not available"});
+            .status(200)
+            .json({ deleted: recipeId});
     } catch (err) {
         return res
             .status(500)
             .json({error: (err as any).message || "Unexpected error"});
     }
+
 }
--- store/store-functions/delete-recipe.ts
+++ store/store-functions/delete-recipe.ts
@@ -1,3 +1,17 @@
+import {getDb} from "../db/get-db";
+
 export const deleteRecipe = async (recipeId: string) => {
-    throw new Error("Not Implemented");
+    const db = await getDb();
+
+    let recipeIndex = -1;
+    if(db.data?.recipes) {
+        recipeIndex = db.data.recipes
+            .findIndex((recipe) => recipe.id === recipeId);
+    }
+
+    if (recipeIndex > -1) {
+        db.data?.recipes.splice(recipeIndex, 1);
+        await db.write();
+    }
+
 }
