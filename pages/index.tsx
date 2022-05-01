484824ced3d7bf15e59a9e5054e79b21647786cc
Irenee Ajeneza <irenee.ajeneza@primarybid.com>
2022-05-01 18:52:19 +0100

    Added recipes list to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,74 @@
 import type {NextPage} from 'next'
-import Link from 'next/link'
 
-import {PageLayout} from "@components";
+import {AddRecipeForm, PageLayout, RecipeList} from "@components";
+import {GetStaticProps} from "next";
+import {findAllRecipes, RecipeModel} from "@store";
+import {useRouter} from "next/router";
 
-const Home: NextPage = () => {
+interface HomePagePros {
+    recipes: RecipeModel[]
+}
+
+const API_HEADER = {
+    headers: {
+        "Content-Type": "application/json"
+    }
+}
+
+const Home: NextPage<HomePagePros> = ({ recipes}) => {
+
+    const router = useRouter();
+
+    const handleAddRecipeFormSubmit = async (recipeName: string) => {
+        const response = await fetch("/api/recipes", {
+            method: "POST",
+            body: JSON.stringify({name: recipeName}),
+            ...API_HEADER
+        });
+
+        if (response.status < 300) {
+            // trigger reload of the recipes list
+            await router.replace((router.asPath));
+        }
+    };
+
+    const handleRecipeDelete = async (recipeId: string) => {
+        const response = await fetch(`/api/recipes/${recipeId}`, {
+            method: "DELETE",
+            ...API_HEADER
+        });
+
+        if (response.status < 300) {
+            // trigger reload of the recipes list
+            await router.replace((router.asPath));
+        }
+    }
     return (
         <PageLayout pageTitle="Shopping Manager" isHomePage>
-            <ul>
-                <li>
-                    <Link href="/recipes">
-                        <a>Recipes</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/meal-planning">
-                        <a>Meal Planning</a>
-                    </Link>
-                </li>
-            </ul>
+            <div>
+                <h2>Meal Planning</h2>
 
+            </div>
+
+            <div>
+                <h2>Recipes</h2>
+
+                <AddRecipeForm onFormSubmit={handleAddRecipeFormSubmit}/>
+                <RecipeList recipes={recipes} onRecipeDelete={handleRecipeDelete}/>
+            </div>
         </PageLayout>
     )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+    const recipes = await findAllRecipes();
+    // TODO: handle fetch error
+    return {
+        props: {
+            recipes,
+        }
+    }
+}
+
 
 export default Home
