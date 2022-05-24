9b1f930ea3fddf616d020f10d5244fa52f56832d
Irenee Ajeneza <irenee.ajeneza@primarybid.com>
2022-05-24 17:13:08 +0100

    Applied foundation-sites style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import "foundation-sites/dist/css/foundation-float.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
