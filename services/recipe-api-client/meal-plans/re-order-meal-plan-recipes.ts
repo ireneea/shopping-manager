659bee23fd356847fd832ba88460be9eb139fd67
Irenee Ajeneza <irenee.ajeneza@primarybid.com>
2022-08-28 13:13:57 +0100

    api code re-formating

--- services/recipe-api-client/meal-plans/re-order-meal-plan-recipes.ts
+++ services/recipe-api-client/meal-plans/re-order-meal-plan-recipes.ts
@@ -1,17 +1,17 @@
-import { BASE_URL } from "@services/recipe-api-client/base-url";
-import { DEFAULT_HTTP_HEADER } from "@services/recipe-api-client/default-http-header";
-import { MealPlanModel } from "@store";
-import { ReOrderMealPlanRecipesInput } from "@services/recipe-api";
+import {BASE_URL} from "@services/recipe-api-client/base-url";
+import {DEFAULT_HTTP_HEADER} from "@services/recipe-api-client/default-http-header";
+import {MealPlanModel} from "@store";
+import {ReOrderMealPlanRecipesInput} from "@services/recipe-api";
 
 export const reOrderMealPlanRecipes = async (
-  input: ReOrderMealPlanRecipesInput
+    input: ReOrderMealPlanRecipesInput
 ) => {
-  const response = await fetch(
-    `${BASE_URL}/meal-plans/${input.mealPlanId}/recipes/re-order`,
-    {
-      method: "POST",
-      body: JSON.stringify(input),
-      ...DEFAULT_HTTP_HEADER,
+    const response = await fetch(
+        `${BASE_URL}/meal-plans/${input.mealPlanId}/recipes/re-order`,
+        {
+            method: "POST",
+            body: JSON.stringify(input),
+            ...DEFAULT_HTTP_HEADER,
     }
   );
 
